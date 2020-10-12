50eb44c0ff6ba823439eaeb2cd5f7790ed5bb6bf
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-10-11 21:48:07 -0400

    Added a function that only executes after the html page has loaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+  newCoolText()
+})
+
+const newCoolText = () => {
+  let paragraph = document.querySelector("p#text")
+
+  paragraph.innerHTML = "This is really cool!"
+}
